6e1cea4d41826e45874cbea4b274121e73e831e5
Bryan Sory <superbee45@gmail.com>
2022-09-26 14:59:55 -0500

    express form added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world!")
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form>
+        <input placeholder="email" />
+        <input placeholder="fName" />
+        <input placeholder="lName" />
+        <input placeholder="DOB" />
+        <button> Sign Up </button>
+      </form>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Listening....')
+})
