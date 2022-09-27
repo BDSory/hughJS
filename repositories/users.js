3a1e78875cbd96a1bce599ef968e7bdc85a5e811
Bryan Sory <superbee45@gmail.com>
2022-09-26 19:15:27 -0500

    all utility methods for user handling done and tested

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,6 @@
 const { create } = require('domain');
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   constructor(filename) {
@@ -24,6 +25,7 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
     const records = await this.getAll();
     records.push(attrs);
     //write the updated 'records array back to this.filename
@@ -33,17 +35,60 @@ class UsersRepository {
   async writeAll(records) {
     await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2))
   }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
+  }
   
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+
+    if(!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for(let record of records) {
+      let found = true;
+
+      for(let key in filters) {
+        if(record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+      if(found) {
+        return record;
+      }
+    }
+  }
 }
 
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
   
-  await repo.create({email: 'test@test.com', password: 'password'});
+  const user = await repo.getOneBy({id : "bf3948e7", password : "myPassword"});
   
-  const users = await repo. getAll();
-  console.log(users);
+  console.log(user);
 }
 
 test();
